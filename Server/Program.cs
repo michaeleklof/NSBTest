cf49bd8175b6f3efdf2b33fc92f97aa536313a41
Michael Eklöf <micke@observably.se>
2023-10-08 22:03:31 +0200

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using NServiceBus;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Extensions.Hosting;
+
+class Program
+{
+
+    const string EndpointName = "Samples.AsyncPages.Server";
+    public static async Task Main()
+    {
+        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
+.AddSource("NServiceBus.*")
+.AddOtlpExporter()
+.Build();
+
+        var endpointConfiguration = new EndpointConfiguration(Console.Title = EndpointName);
+        endpointConfiguration.EnableCallbacks(makesRequests: false);
+        endpointConfiguration.UseTransport(new LearningTransport());
+        endpointConfiguration.EnableOpenTelemetry();
+
+
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop();
+    }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Extensions.Hosting;
+
+class Program
+{
+
+    const string EndpointName = "Samples.AsyncPages.WebApplication";
+    public static void Main()
+    {
+        #region ApplicationStart
+        var builder = WebApplication.CreateBuilder();
+
+        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(EndpointName))
+.AddSource("NServiceBus.*")
+.AddOtlpExporter()
+.Build();
+
+        builder.Host.UseNServiceBus(context =>
+        {
+            var endpointConfiguration = new EndpointConfiguration(EndpointName);
+            endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+            endpointConfiguration.EnableCallbacks();
+            endpointConfiguration.UseTransport(new LearningTransport());
+            endpointConfiguration.EnableOpenTelemetry();
+            return endpointConfiguration;
+        });
+        #endregion
+
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
